6b78e7663a3d27b0fd002cb0ed02524220326cc2
H-Walker <47987019+H-Walker@users.noreply.github.com>
2019-02-25 17:27:13 +0000

    Created FlattenArray.js

---
+++ FlattenArray.js
@@ -0,0 +1,8 @@
+//GitHub tests: Flatten an array
+function flattenArray(array) { 
+	return String(array).split(',').map(Number);
+}
+
+var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]]
+
+console.log(flattenArray(arr))//output: 1,2,1,2,3,4,5,1,2,2
