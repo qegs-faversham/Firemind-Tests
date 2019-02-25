6cd8a7b103cfba32a82485adc5f7fd05368abd51
H-Walker <47987019+H-Walker@users.noreply.github.com>
2019-02-25 17:28:19 +0000

    CloneObject.js

---
+++ CloneObject.js
@@ -0,0 +1,18 @@
+//GitHub tests: Clone object
+function cloneObject(object) {
+  return JSON.parse(JSON.stringify(object));
+}
+
+const expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']};
+
+const obj = cloneObject(expected);
+
+console.log(obj); //output:{ name: 'Ahmed',
+ // age: 27,
+ // skills: [ 'cycling', 'walking', 'eating' ] }
+
+//Showing that obj != expected
+obj.name = 'foo';
+
+console.log(expected.name);//output: Ahmed
+console.log(obj.name); //output: foo
